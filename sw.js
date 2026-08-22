d81c8f462bd07a2fc832b9a8db3be44cdcc68e98
Danish Ekram <danishekram188@gmail.com>
2026-08-22 23:58:01 +0530

    Fix UI lag and enable fast offline pre-caching

--- sw.js
+++ sw.js
@@ -1,11 +1,48 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'student-diary-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/attendance.html',
+  '/details.html',
+  '/percentage.html',
+  '/buildweek.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(e.request).then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+      );
+    })
+  );
 });
